e2d7feb399ce05f117df092f9945e3c700623865
TiaraHafizah23 <2110031805002@sar.ac.id>
2023-03-28 14:06:25 +0700

    Initial commit

---
+++ app/src/main/java/com/tiara/fragmentkotlin/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.tiara.fragmentkotlin
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.f1Fragment, firstFragment)
+            commit()
+        }
+
+        val btnFirstFragment = findViewById<Button>(R.id.btnFirstFragment)
+
+        btnFirstFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.f1Fragment, firstFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
+        val btnSecondFragment= findViewById<Button>(R.id.btnSecondFragment)
+
+        btnSecondFragment.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.f1Fragment, secondFragment)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+}
